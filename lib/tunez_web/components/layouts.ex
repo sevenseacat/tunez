deba9834417e61b8a5ff25ad8c111e82447a0445
Rebecca Le <traybaby@gmail.com>
2024-09-20 17:43:47 +0800

    Initial Phoenix app generated with Igniter

---
+++ lib/tunez_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule TunezWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use TunezWeb, :controller` and
+  `use TunezWeb, :live_view`.
+  """
+  use TunezWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/tunez_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule TunezWeb.PageController do
+  use TunezWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
