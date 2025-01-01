9dba47e99d9ec729b80b63ab9f58dda25fcf6451
Rebecca Le <traybaby@gmail.com>
2025-01-01 23:53:35 +0800

    Chapter 3 part 3 - Pagination of Search Results

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -14,13 +14,19 @@ defmodule TunezWeb.Artists.IndexLive do
   def handle_params(params, _url, socket) do
     sort_by = Map.get(params, "sort_by") |> validate_sort_by()
     query_text = Map.get(params, "q", "")
-    artists = Tunez.Music.search_artists!(query_text, query: [sort_input: sort_by])
+    page_params = AshPhoenix.LiveView.page_from_params(params, 12)
+
+    page =
+      Tunez.Music.search_artists!(query_text,
+        page: page_params,
+        query: [sort_input: sort_by]
+      )
 
     socket =
       socket
       |> assign(:sort_by, sort_by)
       |> assign(:query_text, query_text)
-      |> assign(:artists, artists)
+      |> assign(:page, page)
 
     {:noreply, socket}
   end
@@ -40,16 +46,18 @@ defmodule TunezWeb.Artists.IndexLive do
       </:action>
     </.header>
 
-    <div :if={@artists == []} class="p-8 text-center">
+    <div :if={@page.results == []} class="p-8 text-center">
       <.icon name="hero-face-frown" class="w-32 h-32 bg-base-300" />
       <br /> No artist data to display!
     </div>
 
     <ul class="gap-6 lg:gap-12 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-      <li :for={artist <- @artists}>
+      <li :for={artist <- @page.results}>
         <.artist_card artist={artist} />
       </li>
     </ul>
+
+    <.pagination_links page={@page} query_text={@query_text} sort_by={@sort_by} />
     """
   end
 
@@ -85,11 +93,31 @@ defmodule TunezWeb.Artists.IndexLive do
 
   def pagination_links(assigns) do
     ~H"""
-    <div class="flex justify-center pt-8 join">
-      <.button_link data-role="previous-page" class="join-item" kind="primary" outline>
+    <div
+      :if={
+        AshPhoenix.LiveView.prev_page?(@page) ||
+          AshPhoenix.LiveView.next_page?(@page)
+      }
+      class="flex justify-center pt-8 join"
+    >
+      <.button_link
+        data-role="previous-page"
+        patch={~p"/?#{query_string(@page, @query_text, @sort_by, "prev")}"}
+        disabled={!AshPhoenix.LiveView.prev_page?(@page)}
+        class="join-item"
+        kind="primary"
+        outline
+      >
         « Previous
       </.button_link>
-      <.button_link data-role="next-page" class="join-item" kind="primary" outline>
+      <.button_link
+        data-role="next-page"
+        patch={~p"/?#{query_string(@page, @query_text, @sort_by, "next")}"}
+        disabled={!AshPhoenix.LiveView.next_page?(@page)}
+        class="join-item"
+        kind="primary"
+        outline
+      >
         Next »
       </.button_link>
     </div>
@@ -133,6 +161,16 @@ defmodule TunezWeb.Artists.IndexLive do
       </select>
     </form>
     """
+  end
+
+  def query_string(page, query_text, sort_by, which) do
+    case AshPhoenix.LiveView.page_link_params(page, which) do
+      :invalid -> []
+      list -> list
+    end
+    |> Keyword.put(:q, query_text)
+    |> Keyword.put(:sort_by, sort_by)
+    |> remove_empty()
   end
 
   defp sort_options do
