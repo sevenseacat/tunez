e6d755f916e9ce4b1472b1ba39a0e4215a377037
Rebecca Le <traybaby@gmail.com>
2025-01-02 14:15:10 +0800

    Chapter 6 part 1 - Authorizing API Access for Authentication

--- lib/tunez_web/ash_json_api_router.ex
+++ lib/tunez_web/ash_json_api_router.ex
@@ -1,6 +1,6 @@
 defmodule TunezWeb.AshJsonApiRouter do
   use AshJsonApi.Router,
-    domains: [Module.concat(["Tunez.Music"])],
+    domains: [Module.concat(["Tunez.Music"]), Module.concat(["Tunez.Accounts"])],
     open_api: "/open_api",
     open_api_title: "Tunez API Documentation",
     open_api_version: to_string(Application.spec(:tunez, :vsn))
