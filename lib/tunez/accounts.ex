63e0187edf1bc1031063be9187505dd2806ee5bb
Rebecca Le <rebecca@alembic.com.au>
2025-02-07 17:19:13 +0800

    Chapter 6a: Authorizing API Access for Authentication

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -1,10 +1,37 @@
 defmodule Tunez.Accounts do
-  use Ash.Domain, otp_app: :tunez, extensions: [AshJsonApi.Domain]
+  use Ash.Domain, otp_app: :tunez, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
+
+  graphql do
+    queries do
+      get Tunez.Accounts.User, :sign_in_with_password, :sign_in_with_password do
+        identity false
+        type_name :user_with_token
+      end
+    end
+
+    mutations do
+      create Tunez.Accounts.User, :register_with_password, :register_with_password
+    end
+  end
 
   json_api do
     routes do
       base_route "/users", Tunez.Accounts.User do
-        post :register_with_password, route: "/register"
+        post :register_with_password do
+          route "/register"
+
+          metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end
+        end
+
+        post :sign_in_with_password do
+          route "/sign_in"
+
+          metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end
+        end
       end
     end
   end
