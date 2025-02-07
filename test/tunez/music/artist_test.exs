07fae806861dc00516cbed583b4f61b150e76b70
Rebecca Le <rebecca@alembic.com.au>
2025-02-07 18:20:07 +0800

    Chapter 7: All About Testing

--- test/tunez/music/artist_test.exs
+++ test/tunez/music/artist_test.exs
@@ -4,139 +4,128 @@ defmodule Tunez.Music.ArtistTest do
   alias Tunez.Music, warn: false
 
   describe "Tunez.Music.read_artists!/0-2" do
-    @tag :skip
     test "when there is no data, nothing is returned" do
-      # assert Music.read_artists!() == []
+      assert Music.read_artists!() == []
     end
   end
 
   describe "Tunez.Music.search_artists/1-2" do
     def names(page), do: Enum.map(page.results, & &1.name)
 
-    @tag :skip
     test "can filter by partial name matches" do
-      # ["hello", "goodbye", "what?"]
-      # |> Enum.each(&generate(artist(name: &1)))
+      ["hello", "goodbye", "what?"]
+      |> Enum.each(&generate(artist(name: &1)))
 
-      # assert Enum.sort(names(Music.search_artists!("o"))) == ["goodbye", "hello"]
-      # assert names(Music.search_artists!("oo")) == ["goodbye"]
-      # assert names(Music.search_artists!("he")) == ["hello"]
+      assert Enum.sort(names(Music.search_artists!("o"))) == ["goodbye", "hello"]
+      assert names(Music.search_artists!("oo")) == ["goodbye"]
+      assert names(Music.search_artists!("he")) == ["hello"]
     end
 
-    @tag :skip
     test "can sort by name" do
-      # ["first", "third", "fourth", "second"]
-      # |> Enum.each(&generate(artist(name: &1)))
+      ["first", "third", "fourth", "second"]
+      |> Enum.each(&generate(artist(name: &1)))
 
-      # actual = names(Music.search_artists!("", query: [sort_input: "+name"]))
-      # assert actual == ["first", "fourth", "second", "third"]
+      actual = names(Music.search_artists!("", query: [sort_input: "+name"]))
+      assert actual == ["first", "fourth", "second", "third"]
     end
 
-    @tag :skip
     test "can sort by creation time" do
-      # generate(artist(seed?: true, name: "first", inserted_at: ago(30, :second)))
-      # generate(artist(seed?: true, name: "third", inserted_at: ago(10, :second)))
-      # generate(artist(seed?: true, name: "second", inserted_at: ago(20, :second)))
+      generate(artist(seed?: true, name: "first", inserted_at: ago(30, :second)))
+      generate(artist(seed?: true, name: "third", inserted_at: ago(10, :second)))
+      generate(artist(seed?: true, name: "second", inserted_at: ago(20, :second)))
 
-      # actual = names(Music.search_artists!("", query: [sort_input: "-inserted_at"]))
-      # assert actual == ["third", "second", "first"]
+      actual = names(Music.search_artists!("", query: [sort_input: "-inserted_at"]))
+      assert actual == ["third", "second", "first"]
     end
 
-    @tag :skip
     test "can sort by update time" do
-      # generate(artist(seed?: true, name: "first", updated_at: ago(30, :second)))
-      # generate(artist(seed?: true, name: "third", updated_at: ago(10, :second)))
-      # generate(artist(seed?: true, name: "second", updated_at: ago(20, :second)))
+      generate(artist(seed?: true, name: "first", updated_at: ago(30, :second)))
+      generate(artist(seed?: true, name: "third", updated_at: ago(10, :second)))
+      generate(artist(seed?: true, name: "second", updated_at: ago(20, :second)))
 
-      # actual = names(Music.search_artists!("", query: [sort_input: "-updated_at"]))
-      # assert actual == ["third", "second", "first"]
+      actual = names(Music.search_artists!("", query: [sort_input: "-updated_at"]))
+      assert actual == ["third", "second", "first"]
     end
 
-    @tag :skip
     test "can sort by latest album release" do
-      # first = generate(artist(name: "first"))
-      # generate(album(year_released: 2023, artist_id: first.id))
+      first = generate(artist(name: "first"))
+      generate(album(year_released: 2023, artist_id: first.id))
 
-      # third = generate(artist(name: "third"))
-      # generate(album(year_released: 2008, artist_id: third.id))
+      third = generate(artist(name: "third"))
+      generate(album(year_released: 2008, artist_id: third.id))
 
-      # second = generate(artist(name: "second"))
-      # generate(album(year_released: 2012, artist_id: second.id))
+      second = generate(artist(name: "second"))
+      generate(album(year_released: 2012, artist_id: second.id))
 
-      # actual =
-      #   names(Music.search_artists!("", query: [sort_input: "--latest_album_year_released"]))
+      actual =
+        names(Music.search_artists!("", query: [sort_input: "--latest_album_year_released"]))
 
-      # assert actual == ["first", "second", "third"]
+      assert actual == ["first", "second", "third"]
     end
 
-    @tag :skip
     test "can sort by number of album releases" do
-      # generate(artist(name: "two", album_count: 2))
-      # generate(artist(name: "none"))
-      # generate(artist(name: "one", album_count: 1))
-      # generate(artist(name: "three", album_count: 3))
+      generate(artist(name: "two", album_count: 2))
+      generate(artist(name: "none"))
+      generate(artist(name: "one", album_count: 1))
+      generate(artist(name: "three", album_count: 3))
 
-      # actual =
-      #   names(Music.search_artists!("", query: [sort_input: "-album_count"]))
+      actual =
+        names(Music.search_artists!("", query: [sort_input: "-album_count"]))
 
-      # assert actual == ["three", "two", "one", "none"]
+      assert actual == ["three", "two", "one", "none"]
     end
 
-    @tag :skip
     test "can paginate search results" do
-      # generate_many(artist(), 2)
+      generate_many(artist(), 2)
 
-      # page = Music.search_artists!("", page: [limit: 1])
-      # assert length(page.results) == 1
-      # assert page.more?
+      page = Music.search_artists!("", page: [limit: 1])
+      assert length(page.results) == 1
+      assert page.more?
 
-      # next_page = Ash.page!(page, :next)
-      # assert length(page.results) == 1
-      # refute next_page.more?
+      next_page = Ash.page!(page, :next)
+      assert length(page.results) == 1
+      refute next_page.more?
     end
   end
 
   describe "Tunez.Music.create_artist/1-2" do
-    @tag :skip
     test "stores the actor that created the record" do
-      # actor = generate(user(role: :admin))
+      actor = generate(user(role: :admin))
 
-      # artist = Music.create_artist!(%{name: "New Artist"}, actor: actor)
-      # assert artist.created_by_id == actor.id
-      # assert artist.updated_by_id == actor.id
+      artist = Music.create_artist!(%{name: "New Artist"}, actor: actor)
+      assert artist.created_by_id == actor.id
+      assert artist.updated_by_id == actor.id
     end
   end
 
   describe "Tunez.Music.update_artist/2-3" do
-    @tag :skip
     test "collects old names when the artist name changes" do
-      # actor = generate(user(role: :admin))
+      actor = generate(user(role: :admin))
 
-      # artist = generate(artist(name: "First Name"))
-      # assert artist.previous_names == []
+      artist = generate(artist(name: "First Name"))
+      assert artist.previous_names == []
 
-      # # First Name is moved to previous_names
-      # artist = Music.update_artist!(artist, %{name: "Second Name"}, actor: actor)
-      # assert artist.previous_names == ["First Name"]
+      # First Name is moved to previous_names
+      artist = Music.update_artist!(artist, %{name: "Second Name"}, actor: actor)
+      assert artist.previous_names == ["First Name"]
 
-      # # Second Name is added to previous names
-      # artist = Music.update_artist!(artist, %{name: "Third Name"}, actor: actor)
-      # assert artist.previous_names == ["Second Name", "First Name"]
+      # Second Name is added to previous names
+      artist = Music.update_artist!(artist, %{name: "Third Name"}, actor: actor)
+      assert artist.previous_names == ["Second Name", "First Name"]
 
-      # # First Name is now the current name again, not a previous name
-      # artist = Music.update_artist!(artist, %{name: "First Name"}, actor: actor)
-      # assert artist.previous_names == ["Third Name", "Second Name"]
+      # First Name is now the current name again, not a previous name
+      artist = Music.update_artist!(artist, %{name: "First Name"}, actor: actor)
+      assert artist.previous_names == ["Third Name", "Second Name"]
     end
 
-    @tag :skip
     test "stores the actor that updated the record" do
-      # actor = generate(user(role: :admin))
+      actor = generate(user(role: :admin))
 
-      # artist = generate(artist(name: "First Name"))
-      # refute artist.updated_by_id == actor.id
+      artist = generate(artist(name: "First Name"))
+      refute artist.updated_by_id == actor.id
 
-      # artist = Music.update_artist!(artist, %{name: "Second Name"}, actor: actor)
-      # assert artist.updated_by_id == actor.id
+      artist = Music.update_artist!(artist, %{name: "Second Name"}, actor: actor)
+      assert artist.updated_by_id == actor.id
     end
   end
 
@@ -200,71 +189,67 @@ defmodule Tunez.Music.ArtistTest do
   end
 
   describe "cover_image_url" do
-    @tag :skip
     test "uses the cover from the first album that has a cover" do
-      # artist = generate(artist())
-      # generate(album(artist_id: artist.id, year_released: 2021))
+      artist = generate(artist())
+      generate(album(artist_id: artist.id, year_released: 2021))
 
-      # generate(
-      #   album(
-      #     artist_id: artist.id,
-      #     year_released: 2019,
-      #     cover_image_url: "/images/older.jpg"
-      #   )
-      # )
+      generate(
+        album(
+          artist_id: artist.id,
+          year_released: 2019,
+          cover_image_url: "/images/older.jpg"
+        )
+      )
 
-      # generate(
-      #   album(
-      #     artist_id: artist.id,
-      #     year_released: 2020,
-      #     cover_image_url: "/images/the_real_cover.png"
-      #   )
-      # )
+      generate(
+        album(
+          artist_id: artist.id,
+          year_released: 2020,
+          cover_image_url: "/images/the_real_cover.png"
+        )
+      )
 
-      # {:ok, artist} = Ash.load(artist, :cover_image_url)
-      # assert artist.cover_image_url == "/images/the_real_cover.png"
+      {:ok, artist} = Ash.load(artist, :cover_image_url)
+      assert artist.cover_image_url == "/images/the_real_cover.png"
     end
   end
 
   describe "policies" do
-    # def setup_users do
-    #   %{
-    #     admin: generate(user(role: :admin)),
-    #     editor: generate(user(role: :editor)),
-    #     user: generate(user(role: :user))
-    #   }
-    # end
+    def setup_users do
+      %{
+        admin: generate(user(role: :admin)),
+        editor: generate(user(role: :editor)),
+        user: generate(user(role: :user))
+      }
+    end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "only admins can create new artists" do
-      # users = setup_users()
+      users = setup_users()
 
-      # assert Music.can_create_artist?(users.admin)
-      # refute Music.can_create_artist?(users.editor)
-      # refute Music.can_create_artist?(users.user)
-      # refute Music.can_create_artist?(nil)
+      assert Music.can_create_artist?(users.admin)
+      refute Music.can_create_artist?(users.editor)
+      refute Music.can_create_artist?(users.user)
+      refute Music.can_create_artist?(nil)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "only admins can delete artists" do
-      # users = setup_users()
-      # artist = generate(artist())
+      users = setup_users()
+      artist = generate(artist())
 
-      # assert Music.can_destroy_artist?(users.admin, artist)
-      # refute Music.can_destroy_artist?(users.editor, artist)
-      # refute Music.can_destroy_artist?(users.user, artist)
-      # refute Music.can_destroy_artist?(nil, artist)
+      assert Music.can_destroy_artist?(users.admin, artist)
+      refute Music.can_destroy_artist?(users.editor, artist)
+      refute Music.can_destroy_artist?(users.user, artist)
+      refute Music.can_destroy_artist?(nil, artist)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "admins and editors can update artists" do
-      # users = setup_users()
-      # artist = generate(artist())
+      users = setup_users()
+      artist = generate(artist())
 
-      # assert Music.can_update_artist?(users.admin, artist)
-      # assert Music.can_update_artist?(users.editor, artist)
-      # refute Music.can_update_artist?(users.user, artist)
-      # refute Music.can_update_artist?(nil, artist)
+      assert Music.can_update_artist?(users.admin, artist)
+      assert Music.can_update_artist?(users.editor, artist)
+      refute Music.can_update_artist?(users.user, artist)
+      refute Music.can_update_artist?(nil, artist)
     end
 
     @tag skip: "can be enabled during chapter 9. Also uncomment the `setup_users` function above"
--- test/tunez_web/live/albums/form_live_test.exs
+++ test/tunez_web/live/albums/form_live_test.exs
@@ -4,31 +4,29 @@ defmodule TunezWeb.Albums.FormLiveTest do
   alias Tunez.Music, warn: false
 
   describe "creating a new album" do
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "errors for forbidden users", %{conn: _conn} do
-      # artist = generate(artist())
+    test "errors for forbidden users", %{conn: conn} do
+      artist = generate(artist())
 
-      # assert_raise(Ash.Error.Forbidden, fn ->
-      #   conn
-      #   |> insert_and_authenticate_user()
-      #   |> visit(~p"/artists/#{artist}/albums/new")
-      # end)
+      assert_raise(Ash.Error.Forbidden, fn ->
+        conn
+        |> insert_and_authenticate_user()
+        |> visit(~p"/artists/#{artist}/albums/new")
+      end)
     end
 
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "succeeds when valid details are entered", %{conn: _conn} do
-      # artist = generate(artist())
+    test "succeeds when valid details are entered", %{conn: conn} do
+      artist = generate(artist())
 
-      # conn
-      # |> insert_and_authenticate_user(:admin)
-      # |> visit(~p"/artists/#{artist}/albums/new")
-      # |> fill_in("Name", with: "Final Days")
-      # |> fill_in("Year Released", with: 2021)
-      # |> click_button("Save")
-      # |> assert_has(flash(:info), text: "Album saved successfully")
+      conn
+      |> insert_and_authenticate_user(:admin)
+      |> visit(~p"/artists/#{artist}/albums/new")
+      |> fill_in("Name", with: "Final Days")
+      |> fill_in("Year Released", with: 2021)
+      |> click_button("Save")
+      |> assert_has(flash(:info), text: "Album saved successfully")
 
-      # album = get_by_name!(Tunez.Music.Album, "Final Days")
-      # assert album.artist_id == artist.id
+      album = get_by_name!(Tunez.Music.Album, "Final Days")
+      assert album.artist_id == artist.id
     end
 
     @tag skip: "Can be enabled during chapter 8.
@@ -57,61 +55,57 @@ defmodule TunezWeb.Albums.FormLiveTest do
       # assert ["First Track", "Second Track", "Third Track"] == Enum.map(album.tracks, & &1.name)
     end
 
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "fails when invalid details are entered", %{conn: _conn} do
-      # artist = generate(artist())
+    test "fails when invalid details are entered", %{conn: conn} do
+      artist = generate(artist())
 
-      # conn
-      # |> insert_and_authenticate_user(:admin)
-      # |> visit(~p"/artists/#{artist}/albums/new")
-      # |> fill_in("Name", with: "Missing Year")
-      # |> click_button("Save")
-      # |> assert_has(flash(:error), text: "Could not save album data")
+      conn
+      |> insert_and_authenticate_user(:admin)
+      |> visit(~p"/artists/#{artist}/albums/new")
+      |> fill_in("Name", with: "Missing Year")
+      |> click_button("Save")
+      |> assert_has(flash(:error), text: "Could not save album data")
 
-      # refute get_by_name(Tunez.Music.Artist, "Missing Year")
+      refute get_by_name(Tunez.Music.Artist, "Missing Year")
     end
   end
 
   describe "updating an existing album" do
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "errors for forbidden users", %{conn: _conn} do
-      # album = generate(album())
+    test "errors for forbidden users", %{conn: conn} do
+      album = generate(album())
 
-      # assert_raise(Ash.Error.Forbidden, fn ->
-      #   conn
-      #   |> insert_and_authenticate_user()
-      #   |> visit(~p"/albums/#{album}/edit")
-      # end)
+      assert_raise(Ash.Error.Forbidden, fn ->
+        conn
+        |> insert_and_authenticate_user()
+        |> visit(~p"/albums/#{album}/edit")
+      end)
     end
 
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "succeeds when valid details are entered", %{conn: _conn} do
-      # album = generate(album(name: "Old Name"))
+    test "succeeds when valid details are entered", %{conn: conn} do
+      album = generate(album(name: "Old Name"))
 
-      # conn
-      # |> insert_and_authenticate_user(:admin)
-      # |> visit(~p"/albums/#{album}/edit")
-      # |> fill_in("Name", with: "New Name")
-      # |> click_button("Save")
-      # |> assert_has(flash(:info), text: "Album saved successfully")
+      conn
+      |> insert_and_authenticate_user(:admin)
+      |> visit(~p"/albums/#{album}/edit")
+      |> fill_in("Name", with: "New Name")
+      |> click_button("Save")
+      |> assert_has(flash(:info), text: "Album saved successfully")
 
-      # album = Music.get_album_by_id!(album.id)
-      # assert album.name == "New Name"
+      album = Music.get_album_by_id!(album.id)
+      assert album.name == "New Name"
     end
 
-    @tag skip: "Also need to change `_conn` to `conn` below"
-    test "fails when invalid details are entered", %{conn: _conn} do
-      # album = generate(album(name: "Old Name"))
+    test "fails when invalid details are entered", %{conn: conn} do
+      album = generate(album(name: "Old Name"))
 
-      # conn
-      # |> insert_and_authenticate_user(:admin)
-      # |> visit(~p"/albums/#{album}/edit")
-      # |> fill_in("Name", with: "")
-      # |> click_button("Save")
-      # |> assert_has(flash(:error), text: "Could not save album data")
+      conn
+      |> insert_and_authenticate_user(:admin)
+      |> visit(~p"/albums/#{album}/edit")
+      |> fill_in("Name", with: "")
+      |> click_button("Save")
+      |> assert_has(flash(:error), text: "Could not save album data")
 
-      # album = Music.get_album_by_id!(album.id)
-      # assert album.name == "Old Name"
+      album = Music.get_album_by_id!(album.id)
+      assert album.name == "Old Name"
     end
   end
 end
