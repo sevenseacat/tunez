8a98ea6704ca3494bdc2e9d237c8b3f3d17e067b
Rebecca Le <traybaby@gmail.com>
2025-04-16 17:20:58 +0800

    Chapter 1c. Integrating Actions into LiveViews

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ash_phoenix, "~> 2.0"},
       {:ash_postgres, "~> 2.0"},
       {:sourceror, "~> 1.8", only: [:dev, :test]},
       {:ash, "~> 3.0"},
