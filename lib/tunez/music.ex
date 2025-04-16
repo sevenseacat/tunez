f4fe26274a55e9d5f0bb5fc34fbfee10f1c79898
Rebecca Le <traybaby@gmail.com>
2025-04-16 17:55:14 +0800

    Chapter 2a. Resources and Relationships

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -1,6 +1,10 @@
 defmodule Tunez.Music do
   use Ash.Domain, otp_app: :tunez, extensions: [AshPhoenix]
 
+  forms do
+    form :create_album, args: [:artist_id]
+  end
+
   resources do
     resource Tunez.Music.Artist do
       define :create_artist, action: :create
@@ -8,6 +12,13 @@ defmodule Tunez.Music do
       define :get_artist_by_id, action: :read, get_by: :id
       define :update_artist, action: :update
       define :destroy_artist, action: :destroy
+    end
+
+    resource Tunez.Music.Album do
+      define :create_album, action: :create
+      define :get_album_by_id, action: :read, get_by: :id
+      define :update_album, action: :update
+      define :destroy_album, action: :destroy
     end
   end
 end
---
+++ lib/tunez/music/album.ex
@@ -0,0 +1,47 @@
+defmodule Tunez.Music.Album do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "albums"
+    repo Tunez.Repo
+
+    references do
+      reference :artist, index?: true
+    end
+  end
+
+  actions do
+    defaults [:read, :destroy]
+
+    create :create do
+      accept [:name, :year_released, :cover_image_url, :artist_id]
+    end
+
+    update :update do
+      accept [:name, :year_released, :cover_image_url]
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+    end
+
+    attribute :cover_image_url, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :artist, Tunez.Music.Artist do
+      allow_nil? false
+    end
+  end
+end
