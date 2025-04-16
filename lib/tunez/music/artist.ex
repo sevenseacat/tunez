6f012c3452afa3aeff88207f45e2ee1f2c4ba076
Rebecca Le <traybaby@gmail.com>
2025-04-16 22:44:38 +0800

    Chapter 3b. Dynamically Sorting Artists

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -36,6 +36,7 @@ defmodule Tunez.Music.Artist do
 
     attribute :name, :string do
       allow_nil? false
+      public? true
     end
 
     attribute :biography, :string
@@ -44,8 +45,8 @@ defmodule Tunez.Music.Artist do
       default []
     end
 
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
+    create_timestamp :inserted_at, public?: true
+    update_timestamp :updated_at, public?: true
   end
 
   relationships do
