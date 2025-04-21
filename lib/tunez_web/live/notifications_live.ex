b4a52fb46b0de617e7351754bf3a52dba5dcb619
Rebecca Le <traybaby@gmail.com>
2025-04-21 16:57:48 +0800

    Chapter 10c. Showing Notifications to Users

--- lib/tunez_web/live/notifications_live.ex
+++ lib/tunez_web/live/notifications_live.ex
@@ -1,8 +1,10 @@
 defmodule TunezWeb.NotificationsLive do
   use TunezWeb, :live_view
 
+  on_mount {TunezWeb.LiveUserAuth, :current_user}
+
   def mount(_params, _session, socket) do
-    notifications = []
+    notifications = Tunez.Accounts.notifications_for_user!(actor: socket.assigns.current_user)
     {:ok, assign(socket, notifications: notifications)}
   end
 
@@ -56,7 +58,12 @@ defmodule TunezWeb.NotificationsLive do
     """
   end
 
-  def handle_event("dismiss-notification", %{"id" => _id}, socket) do
-    {:noreply, socket}
+  def handle_event("dismiss-notification", %{"id" => id}, socket) do
+    notification = Enum.find(socket.assigns.notifications, &(&1.id == id))
+
+    Tunez.Accounts.dismiss_notification(notification, actor: socket.assigns.current_user)
+
+    notifications = Enum.reject(socket.assigns.notifications, &(&1.id == id))
+    {:noreply, assign(socket, notifications: notifications)}
   end
 end
