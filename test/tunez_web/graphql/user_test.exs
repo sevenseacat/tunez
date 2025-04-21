e24a16ef93a2852ba7c216df9d26e34566532625
Rebecca Le <traybaby@gmail.com>
2025-04-21 12:21:49 +0800

    Chapter 7. All About Testing

--- test/tunez_web/graphql/user_test.exs
+++ test/tunez_web/graphql/user_test.exs
@@ -2,56 +2,54 @@ defmodule TunezWeb.Graphql.UserTest do
   use TunezWeb.ConnCase, async: true
 
   describe "queries" do
-    @tag :skip
     test "signInUser" do
-      # user = generate(user(email: "test@test.com", password: "password"))
+      user = generate(user(email: "test@test.com", password: "password"))
 
-      # assert {:ok, resp} =
-      #          """
-      #          query signInUser($email: String!, $password: String!) {
-      #            signInUser(email: $email, password: $password) {
-      #              id
-      #              token
-      #            }
-      #          }
-      #          """
-      #          |> Absinthe.run(TunezWeb.GraphqlSchema,
-      #            variables: %{"email" => "test@test.com", "password" => "password"}
-      #          )
+      assert {:ok, resp} =
+               """
+               query signInUser($email: String!, $password: String!) {
+                 signInUser(email: $email, password: $password) {
+                   id
+                   token
+                 }
+               }
+               """
+               |> Absinthe.run(TunezWeb.GraphqlSchema,
+                 variables: %{"email" => "test@test.com", "password" => "password"}
+               )
 
-      # result = resp.data["signInUser"]
-      # assert result["id"] == user.id
-      # assert result["token"] != nil
+      result = resp.data["signInUser"]
+      assert result["id"] == user.id
+      assert result["token"] != nil
     end
   end
 
   describe "mutations" do
-    @tag :skip
     test "registerUser" do
-      # assert {:ok, resp} =
-      #          """
-      #          mutation registerUser($input: RegisterUserInput!) {
-      #            registerUser(input: $input) {
-      #              errors { message }
-      #              metadata { token }
-      #              result { id }
-      #            }
-      #          }
-      #          """
-      #          |> Absinthe.run(TunezWeb.GraphqlSchema,
-      #            variables: %{
-      #              "input" => %{
-      #                "email" => "test2@test.com",
-      #                "password" => "password2",
-      #                "passwordConfirmation" => "password2"
-      #              }
-      #            }
-      #          )
+      assert {:ok, resp} =
+               """
+               mutation registerUser($input: RegisterUserInput!) {
+                 registerUser(input: $input) {
+                   errors { message }
+                   metadata { token }
+                   result { id }
+                 }
+               }
+               """
+               |> Absinthe.run(TunezWeb.GraphqlSchema,
+                 variables: %{
+                   "input" => %{
+                     "email" => "test2@test.com",
+                     "password" => "password2",
+                     "passwordConfirmation" => "password2"
+                   }
+                 }
+               )
 
-      # data = resp.data["registerUser"]
-      # assert Enum.empty?(data["errors"])
-      # assert data["metadata"]["token"] != nil
-      # assert data["result"]["id"] != nil
+      data = resp.data["registerUser"]
+      assert Enum.empty?(data["errors"])
+      assert data["metadata"]["token"] != nil
+      assert data["result"]["id"] != nil
     end
   end
 end
