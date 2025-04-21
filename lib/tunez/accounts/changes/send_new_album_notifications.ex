798e67babb550393d15ec6c9e6edce5c756a258a
Rebecca Le <traybaby@gmail.com>
2025-04-21 16:37:23 +0800

    Chapter 10a. Notifying Users about New Albums

---
+++ lib/tunez/accounts/changes/send_new_album_notifications.ex
@@ -0,0 +1,9 @@
+defmodule Tunez.Accounts.Changes.SendNewAlbumNotifications do
+  use Ash.Resource.Change
+
+  @impl true
+  def change(changeset, _opts, _context) do
+    # Create notifications here!
+    changeset
+  end
+end
