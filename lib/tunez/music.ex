404b52cbc403bd2ba76188c445f23a8a461f6c72
Rebecca Le <traybaby@gmail.com>
2025-07-19 15:04:32 +0800

    Chapter 1b. Oh, CRUD! — Defining Basic Actions

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -3,6 +3,12 @@ defmodule Tunez.Music do
     otp_app: :tunez
 
   resources do
-    resource Tunez.Music.Artist
+    resource Tunez.Music.Artist do
+      define :create_artist, action: :create
+      define :read_artists, action: :read
+      define :get_artist_by_id, action: :read, get_by: :id
+      define :update_artist, action: :update
+      define :destroy_artist, action: :destroy
+    end
   end
 end
