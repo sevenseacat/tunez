7f3bf4a2f844bceabafcaa7bf9a4982ad8fe7f38
Rebecca Le <traybaby@gmail.com>
2025-07-19 15:39:49 +0800

    Chapter 1c. Integrating Actions into LiveViews

--- lib/tunez_web/endpoint.ex
+++ lib/tunez_web/endpoint.ex
@@ -31,6 +31,7 @@ defmodule TunezWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
+    plug AshPhoenix.Plug.CheckCodegenStatus
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :tunez
   end
 
