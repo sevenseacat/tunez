1f41e8ec901ffb592c64c2938e42122c6fb819ca
Rebecca Le <traybaby@gmail.com>
2025-07-19 23:03:41 +0800

    Chapter 5b. Setting Up Password Authentication

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:picosat_elixir, "~> 0.2"},
       {:ash_authentication, "~> 4.0"},
       {:absinthe_phoenix, "~> 2.0"},
