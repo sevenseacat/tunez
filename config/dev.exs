3f9344c6bcf9b92fedbd7b9b4e84c4d3b66333c1
Rebecca Le <traybaby@gmail.com>
2025-07-19 23:34:54 +0800

    Chapter 5d. Setting Up Magic Link Authentication

--- config/dev.exs
+++ config/dev.exs
@@ -84,3 +84,5 @@ config :phoenix_live_view,
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
+
+config :ash_authentication, debug_authentication_failures?: true
--- lib/tunez/accounts/user.ex
+++ lib/tunez/accounts/user.ex
@@ -4,7 +4,7 @@ defmodule Tunez.Accounts.User do
     domain: Tunez.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshAuthentication]
+    extensions: [AshJsonApi.Resource, AshAuthentication]
 
   authentication do
     add_ons do
@@ -43,7 +43,19 @@ defmodule Tunez.Accounts.User do
           request_password_reset_action_name :request_password_reset_token
         end
       end
+
+      magic_link do
+        identity_field :email
+        registration_enabled? true
+        require_interaction? true
+
+        sender Tunez.Accounts.User.Senders.SendMagicLinkEmail
+      end
     end
+  end
+
+  json_api do
+    type "user"
   end
 
   postgres do
@@ -226,6 +238,34 @@ defmodule Tunez.Accounts.User do
       # Generates an authentication token for the user
       change AshAuthentication.GenerateTokenChange
     end
+
+    create :sign_in_with_magic_link do
+      description "Sign in or register a user with magic link."
+
+      argument :token, :string do
+        description "The token from the magic link that was sent to the user"
+        allow_nil? false
+      end
+
+      upsert? true
+      upsert_identity :unique_email
+      upsert_fields [:email]
+
+      # Uses the information from the token to create or sign in the user
+      change AshAuthentication.Strategy.MagicLink.SignInChange
+
+      metadata :token, :string do
+        allow_nil? false
+      end
+    end
+
+    action :request_magic_link do
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      run AshAuthentication.Strategy.MagicLink.Request
+    end
   end
 
   policies do
@@ -247,7 +287,6 @@ defmodule Tunez.Accounts.User do
     end
 
     attribute :hashed_password, :string do
-      allow_nil? false
       sensitive? true
     end
 
