a59a2b19bb4ce65dc17ff5f245ee8fcd6a3c8555
Rebecca Le <traybaby@gmail.com>
2025-07-19 23:50:04 +0800

    Chapter 6b. Authorizing API Access for Authentication

--- lib/tunez/accounts/user.ex
+++ lib/tunez/accounts/user.ex
@@ -4,7 +4,7 @@ defmodule Tunez.Accounts.User do
     domain: Tunez.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshJsonApi.Resource, AshAuthentication]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource, AshAuthentication]
 
   authentication do
     add_ons do
@@ -52,6 +52,10 @@ defmodule Tunez.Accounts.User do
         sender Tunez.Accounts.User.Senders.SendMagicLinkEmail
       end
     end
+  end
+
+  graphql do
+    type :user
   end
 
   json_api do
@@ -273,8 +277,8 @@ defmodule Tunez.Accounts.User do
       authorize_if always()
     end
 
-    policy always() do
-      forbid_if always()
+    policy action([:register_with_password, :sign_in_with_password]) do
+      authorize_if always()
     end
   end
 
