81078dc05cf42fa80945c33b6f90debab76700b2
Rebecca Le <traybaby@gmail.com>
2025-07-20 19:14:19 +0800

    Chapter 10e. We Need to Talk About Atomics

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -56,10 +56,8 @@ defmodule Tunez.Music.Artist do
     end
 
     update :update do
-      require_atomic? false
       accept [:name, :biography]
-
-      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
+      change Tunez.Music.Changes.UpdatePreviousNames
     end
 
     destroy :destroy do
