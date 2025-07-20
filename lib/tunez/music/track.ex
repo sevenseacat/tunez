e4f670c4d9bd5b752a432c735b74c12a79be4ab8
Rebecca Le <traybaby@gmail.com>
2025-07-20 17:04:32 +0800

    Chapter 8e. Adding Track Data to API Responses

--- lib/tunez/music/track.ex
+++ lib/tunez/music/track.ex
@@ -3,7 +3,19 @@ defmodule Tunez.Music.Track do
     otp_app: :tunez,
     domain: Tunez.Music,
     data_layer: AshPostgres.DataLayer,
-    authorizers: [Ash.Policy.Authorizer]
+    authorizers: [Ash.Policy.Authorizer],
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :track
+    derive_filter? false
+    derive_sort? false
+  end
+
+  json_api do
+    type "track"
+    default_fields [:number, :name, :duration]
+  end
 
   postgres do
     table "tracks"
@@ -53,6 +65,7 @@ defmodule Tunez.Music.Track do
 
     attribute :name, :string do
       allow_nil? false
+      public? true
     end
 
     attribute :duration_seconds, :integer do
@@ -71,7 +84,12 @@ defmodule Tunez.Music.Track do
   end
 
   calculations do
-    calculate :number, :integer, expr(order + 1)
-    calculate :duration, :string, Tunez.Music.Calculations.SecondsToMinutes
+    calculate :number, :integer, expr(order + 1) do
+      public? true
+    end
+
+    calculate :duration, :string, Tunez.Music.Calculations.SecondsToMinutes do
+      public? true
+    end
   end
 end
