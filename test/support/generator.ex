d44df86cbf3efdc1b5b6cabdd6c12119fedf3228
Rebecca Le <traybaby@gmail.com>
2025-07-20 14:20:53 +0800

    Chapter 7. All About Testing

--- test/support/generator.ex
+++ test/support/generator.ex
@@ -15,54 +15,53 @@ defmodule Tunez.Generator do
   - `:artist_id` - Specify the artist ID for the album.
   """
   def album(opts \\ []) do
-    raise "Uncomment the `album` generator content in `test/support/generator.ex` (and remove this line)"
-    # actor =
-    #   opts[:actor] ||
-    #     once(:default_actor, fn ->
-    #       generate(user(role: opts[:actor_role] || :editor))
-    #     end)
+    actor =
+      opts[:actor] ||
+        once(:default_actor, fn ->
+          generate(user(role: opts[:actor_role] || :editor))
+        end)
 
-    # artist_id =
-    #   opts[:artist_id] ||
-    #     once(:default_artist_id, fn ->
-    #       generate(artist()).id
-    #     end)
+    artist_id =
+      opts[:artist_id] ||
+        once(:default_artist_id, fn ->
+          generate(artist()).id
+        end)
 
-    # after_action =
-    #   if opts[:track_count] do
-    #     fn album ->
-    #       raise "Uncomment the `track_count` after action for the album generator in `test/support/generator.ex` (and remove this line)"
-    #       # generate_many(track(seed?: true, album_id: album.id), opts[:track_count])
-    #       # Ash.load!(album, :tracks)
-    #     end
-    #   end
+    after_action =
+      if opts[:track_count] do
+        fn album ->
+          raise "Uncomment the `track_count` after action for the album generator in `test/support/generator.ex` (and remove this line)"
+          # generate_many(track(seed?: true, album_id: album.id), opts[:track_count])
+          # Ash.load!(album, :tracks)
+        end
+      end
 
-    # if opts[:seed?] do
-    #   seed_generator(
-    #     %Tunez.Music.Album{
-    #       name: sequence(:album_name, &"Album #{&1}"),
-    #       year_released: StreamData.integer(1951..2024),
-    #       artist_id: artist_id
-    #     },
-    #     actor: actor,
-    #     overrides: opts,
-    #     after_action: after_action
-    #   )
-    # else
-    #   changeset_generator(
-    #     Tunez.Music.Album,
-    #     :create,
-    #     defaults: [
-    #       name: sequence(:album_name, &"Album #{&1}"),
-    #       year_released: StreamData.integer(1951..2024),
-    #       artist_id: artist_id,
-    #       cover_image_url: nil
-    #     ],
-    #     overrides: opts,
-    #     actor: actor,
-    #     after_action: after_action
-    #   )
-    # end
+    if opts[:seed?] do
+      seed_generator(
+        %Tunez.Music.Album{
+          name: sequence(:album_name, &"Album #{&1}"),
+          year_released: StreamData.integer(1951..2024),
+          artist_id: artist_id
+        },
+        actor: actor,
+        overrides: opts,
+        after_action: after_action
+      )
+    else
+      changeset_generator(
+        Tunez.Music.Album,
+        :create,
+        defaults: [
+          name: sequence(:album_name, &"Album #{&1}"),
+          year_released: StreamData.integer(1951..2024),
+          artist_id: artist_id,
+          cover_image_url: nil
+        ],
+        overrides: opts,
+        actor: actor,
+        after_action: after_action
+      )
+    end
   end
 
   @doc """
@@ -75,38 +74,37 @@ defmodule Tunez.Generator do
   - `:album_count` - The number of albums to generate for the artist
   """
   def artist(opts \\ []) do
-    raise "Uncomment the `artist` generator content in `test/support/generator.ex` (and remove this line)"
-    # actor =
-    #   opts[:actor] ||
-    #     once(:default_actor, fn ->
-    #       generate(user(role: :admin))
-    #     end)
+    actor =
+      opts[:actor] ||
+        once(:default_actor, fn ->
+          generate(user(role: :admin))
+        end)
 
-    # after_action =
-    #   if opts[:album_count] do
-    #     fn artist ->
-    #       generate_many(album(artist_id: artist.id), opts[:album_count])
-    #       Ash.load!(artist, :albums)
-    #     end
-    #   end
+    after_action =
+      if opts[:album_count] do
+        fn artist ->
+          generate_many(album(artist_id: artist.id), opts[:album_count])
+          Ash.load!(artist, :albums)
+        end
+      end
 
-    # if opts[:seed?] do
-    #   seed_generator(
-    #     %Tunez.Music.Artist{name: sequence(:artist_name, &"Artist #{&1}")},
-    #     actor: actor,
-    #     overrides: opts,
-    #     after_action: after_action
-    #   )
-    # else
-    #   changeset_generator(
-    #     Tunez.Music.Artist,
-    #     :create,
-    #     defaults: [name: sequence(:artist_name, &"Artist #{&1}")],
-    #     actor: actor,
-    #     overrides: opts,
-    #     after_action: after_action
-    #   )
-    # end
+    if opts[:seed?] do
+      seed_generator(
+        %Tunez.Music.Artist{name: sequence(:artist_name, &"Artist #{&1}")},
+        actor: actor,
+        overrides: opts,
+        after_action: after_action
+      )
+    else
+      changeset_generator(
+        Tunez.Music.Artist,
+        :create,
+        defaults: [name: sequence(:artist_name, &"Artist #{&1}")],
+        actor: actor,
+        overrides: opts,
+        after_action: after_action
+      )
+    end
   end
 
   @doc """
@@ -178,23 +176,22 @@ defmodule Tunez.Generator do
   - `:role` - Specify a role to give the created user. Defaults to `:user`.
   """
   def user(opts \\ []) do
-    raise "Uncomment the `user` generator content in `test/support/generator.ex` (and remove this line)"
-    # changeset_generator(
-    #   Tunez.Accounts.User,
-    #   :register_with_password,
-    #   defaults: [
-    #     # Generates unique values using an auto-incrementing sequence
-    #     # eg. `user1@example.com`, `user2@example.com`, etc.
-    #     email: sequence(:user_email, &"user#{&1}@example.com"),
-    #     password: "password",
-    #     password_confirmation: "password"
-    #   ],
-    #   overrides: opts,
-    #   after_action: fn user ->
-    #     role = opts[:role] || :user
-    #     Tunez.Accounts.set_user_role!(user, role, authorize?: false)
-    #   end
-    # )
+    changeset_generator(
+      Tunez.Accounts.User,
+      :register_with_password,
+      defaults: [
+        # Generates unique values using an auto-incrementing sequence
+        # eg. `user1@example.com`, `user2@example.com`, etc.
+        email: sequence(:user_email, &"user#{&1}@example.com"),
+        password: "password",
+        password_confirmation: "password"
+      ],
+      overrides: opts,
+      after_action: fn user ->
+        role = opts[:role] || :user
+        Tunez.Accounts.set_user_role!(user, role, authorize?: false)
+      end
+    )
   end
 
   def duration do
--- test/tunez/music/album_test.exs
+++ test/tunez/music/album_test.exs
@@ -5,19 +5,18 @@ defmodule TunezWeb.Music.AlbumTest do
   alias Tunez.Music, warn: false
 
   describe "Tunez.Music.create_album/1-2" do
-    @tag :skip
     test "stores the actor that created the record" do
-      # actor = generate(user(role: :admin))
-      # artist = generate(artist())
+      actor = generate(user(role: :admin))
+      artist = generate(artist())
 
-      # album =
-      #   Music.create_album!(
-      #     %{name: "New Album", artist_id: artist.id, year_released: 2024},
-      #     actor: actor
-      #   )
+      album =
+        Music.create_album!(
+          %{name: "New Album", artist_id: artist.id, year_released: 2024},
+          actor: actor
+        )
 
-      # assert album.created_by_id == actor.id
-      # assert album.updated_by_id == actor.id
+      assert album.created_by_id == actor.id
+      assert album.updated_by_id == actor.id
     end
 
     @tag skip: "can be enabled during chapter 10"
@@ -50,15 +49,14 @@ defmodule TunezWeb.Music.AlbumTest do
   end
 
   describe "Tunez.Music.update_album/2-3" do
-    @tag :skip
     test "stores the actor that updated the record" do
-      # actor = generate(user(role: :admin))
+      actor = generate(user(role: :admin))
 
-      # album = generate(album(name: "The Old Name"))
-      # refute album.updated_by_id == actor.id
+      album = generate(album(name: "The Old Name"))
+      refute album.updated_by_id == actor.id
 
-      # album = Music.update_album!(album, %{name: "The New Name"}, actor: actor)
-      # assert album.updated_by_id == actor.id
+      album = Music.update_album!(album, %{name: "The New Name"}, actor: actor)
+      assert album.updated_by_id == actor.id
     end
   end
 
@@ -94,114 +92,107 @@ defmodule TunezWeb.Music.AlbumTest do
   end
 
   describe "policies" do
-    # def setup_users do
-    #   %{
-    #     admin: generate(user(role: :admin)),
-    #     editor: generate(user(role: :editor)),
-    #     user: generate(user(role: :user))
-    #   }
-    # end
+    def setup_users do
+      %{
+        admin: generate(user(role: :admin)),
+        editor: generate(user(role: :editor)),
+        user: generate(user(role: :user))
+      }
+    end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "admins and editors can create new albums" do
-      # users = setup_users()
-      # assert Music.can_create_album?(users.admin)
-      # assert Music.can_create_album?(users.editor)
-      # refute Music.can_create_album?(users.user)
-      # refute Music.can_create_album?(nil)
+      users = setup_users()
+      assert Music.can_create_album?(users.admin)
+      assert Music.can_create_album?(users.editor)
+      refute Music.can_create_album?(users.user)
+      refute Music.can_create_album?(nil)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "admins can delete albums" do
-      # users = setup_users()
-      # album = generate(album())
+      users = setup_users()
+      album = generate(album())
 
-      # assert Music.can_destroy_album?(users.admin, album)
-      # refute Music.can_destroy_album?(users.user, album)
-      # refute Music.can_destroy_album?(nil, album)
+      assert Music.can_destroy_album?(users.admin, album)
+      refute Music.can_destroy_album?(users.user, album)
+      refute Music.can_destroy_album?(nil, album)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "admins can update albums" do
-      # users = setup_users()
-      # album = generate(album())
+      users = setup_users()
+      album = generate(album())
 
-      # assert Music.can_update_album?(users.admin, album)
-      # refute Music.can_update_album?(users.user, album)
-      # refute Music.can_update_album?(nil, album)
+      assert Music.can_update_album?(users.admin, album)
+      refute Music.can_update_album?(users.user, album)
+      refute Music.can_update_album?(nil, album)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "editors can edit albums that they created" do
-      # users = setup_users()
-      # can_edit = generate(album(seed?: true, created_by: users.editor))
-      # cant_edit = generate(album(seed?: true, created_by: users.admin))
+      users = setup_users()
+      can_edit = generate(album(seed?: true, created_by: users.editor))
+      cant_edit = generate(album(seed?: true, created_by: users.admin))
 
-      # assert Music.can_update_album?(users.editor, can_edit)
-      # refute Music.can_update_album?(users.editor, cant_edit)
+      assert Music.can_update_album?(users.editor, can_edit)
+      refute Music.can_update_album?(users.editor, cant_edit)
     end
 
-    @tag skip: "Also uncomment the `setup_users` function above"
     test "editors can delete albums that they created" do
-      # users = setup_users()
-      # can_delete = generate(album(seed?: true, created_by: users.editor))
-      # cant_delete = generate(album(seed?: true, created_by: users.admin))
+      users = setup_users()
+      can_delete = generate(album(seed?: true, created_by: users.editor))
+      cant_delete = generate(album(seed?: true, created_by: users.admin))
 
-      # assert Music.can_destroy_album?(users.editor, can_delete)
-      # refute Music.can_destroy_album?(users.editor, cant_delete)
+      assert Music.can_destroy_album?(users.editor, can_delete)
+      refute Music.can_destroy_album?(users.editor, cant_delete)
     end
   end
 
   describe "validations" do
-    @tag :skip
     test "year_released must be between 1950 and next year" do
-      # admin = generate(user(role: :admin))
-      # artist = generate(artist())
+      admin = generate(user(role: :admin))
+      artist = generate(artist())
 
-      # # The assertion isn't really needed here, but we want to signal to
-      # # our future selves that this is part of the test, not the setup.
-      # assert %{artist_id: artist.id, name: "test 2024", year_released: 2024}
-      #        |> Music.create_album!(actor: admin)
+      # The assertion isn't really needed here, but we want to signal to
+      # our future selves that this is part of the test, not the setup.
+      assert %{artist_id: artist.id, name: "test 2024", year_released: 2024}
+             |> Music.create_album!(actor: admin)
 
-      # # Using `assert_raise`
-      # assert_raise Ash.Error.Invalid, ~r/must be between 1950 and next year/, fn ->
-      #   %{artist_id: artist.id, name: "test 1925", year_released: 1925}
-      #   |> Music.create_album!(actor: admin)
-      # end
+      # Using `assert_raise`
+      assert_raise Ash.Error.Invalid, ~r/must be between 1950 and next year/, fn ->
+        %{artist_id: artist.id, name: "test 1925", year_released: 1925}
+        |> Music.create_album!(actor: admin)
+      end
 
-      # # Using `assert_has_error` - note the lack of bang to return the error
-      # %{artist_id: artist.id, name: "test 1950", year_released: 1950}
-      # |> Music.create_album(actor: admin)
-      # |> Ash.Test.assert_has_error(Ash.Error.Invalid, fn error ->
-      #   match?(%{message: "must be between 1950 and next year"}, error)
-      # end)
+      # Using `assert_has_error` - note the lack of bang to return the error
+      %{artist_id: artist.id, name: "test 1950", year_released: 1950}
+      |> Music.create_album(actor: admin)
+      |> Ash.Test.assert_has_error(Ash.Error.Invalid, fn error ->
+        match?(%{message: "must be between 1950 and next year"}, error)
+      end)
     end
 
-    @tag :skip
     test "cover_image_url must be either a remote URL or a local URL from /images" do
-      # admin = generate(user(role: :admin))
-      # artist = generate(artist())
+      admin = generate(user(role: :admin))
+      artist = generate(artist())
 
-      # with_url = fn url ->
-      #   Ash.Generator.action_input(Tunez.Music.Album, :create,
-      #     artist_id: artist.id,
-      #     year_released: 2025,
-      #     cover_image_url: url
-      #   )
-      #   |> Enum.at(0)
-      # end
+      with_url = fn url ->
+        Ash.Generator.action_input(Tunez.Music.Album, :create,
+          artist_id: artist.id,
+          year_released: 2025,
+          cover_image_url: url
+        )
+        |> Enum.at(0)
+      end
 
-      # assert Music.create_album!(with_url.("/images/test.jpg"), actor: admin)
+      assert Music.create_album!(with_url.("/images/test.jpg"), actor: admin)
 
-      # assert_raise Ash.Error.Invalid, ~r/must start with/, fn ->
-      #   Music.create_album!(with_url.("notavalidURL"), actor: admin)
-      # end
+      assert_raise Ash.Error.Invalid, ~r/must start with/, fn ->
+        Music.create_album!(with_url.("notavalidURL"), actor: admin)
+      end
 
-      # with_url.("/image/tunez.mp3")
-      # |> Music.create_album(actor: admin)
-      # |> assert_has_error(fn error ->
-      #   error.field == :cover_image_url && error.message == "must start with https:// or /images/"
-      # end)
+      with_url.("/image/tunez.mp3")
+      |> Music.create_album(actor: admin)
+      |> assert_has_error(fn error ->
+        error.field == :cover_image_url && error.message == "must start with https:// or /images/"
+      end)
     end
   end
 end
